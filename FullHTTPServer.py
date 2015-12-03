fa0070027534a1d7cfcc3f88e441b17709b1f525
merajpatel <patelmu2@mcmaster.ca>
2015-12-03 18:38:58 -0500

    made some changes

--- FullHTTPServer.py
+++ FullHTTPServer.py
@@ -1,36 +1,37 @@
 #!/usr/bin/env python
 
+#import these libraries
 import socket
 import threading 
 import signal
 import sys
 import os
 
+#create WebServer class
 class WebServer:
-	def __init__(self, port, ip):
-		self.port = port
-		self.ip = ip
 
-	def StartServer(self):
-		tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		tcpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) 
-		tcpSocket.bind(("192.168.2.23",80))
+	def StartServer(self):#class constructor
+		tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)#AF_INET refers to IPV4 addressing
+								             #SOCK_STREAM refers to TCP two way connection
+		tcpSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)#SOL_SOCKET is used to set socket level options
+									       #SO_REUSEADDR infers reusability of socket
+		tcpSocket.bind(("192.168.2.23",80))#Replace IP with your LAN IP
 		tcpSocket.listen(1)
-		(clientFD, (ip, port)) = tcpSocket.accept()
-		t = threading.Thread(target=self.handle,args = (clientFD, (ip,port)))
-		t.start()
+		(clientFD, (ip, port)) = tcpSocket.accept()#got client
+		t = threading.Thread(target=self.handle,args = (clientFD, (ip,port)))#make thread to server client
+		t.start()#start thread
 
 	def handle(self, clientFD, clientAddr):
 		print "Connection Recieved form: " + "IP: " + str(clientAddr[0]) + "port:" + str(clientAddr[1])
         	try:
-			data = clientFD.recv(2048)
-        		filename = data.split()[1]
+			data = clientFD.recv(2048)#Read client request
+        		filename = data.split()[1]#Split request, save only resource request. Discard metadata
 			print "file name is:" + filename
-			if filename[1:].find("/") == -1:
+			if filename[1:].find("/") == -1:#find out if file or directory
 				self.displayFile(filename[1:],clientFD)
 			else: 
 				self.displayDir(filename[1:],clientFD)
-        	except IOError:	
+        	except IOError:	#write back invalid URL if error caught
 			clientFD.send("""<html>
                         <head>
                         <title>404 Bad Request</title>
@@ -44,9 +45,9 @@ class WebServer:
 	
 	def displayDir(self, dirname, clientFD):
 		try:
-			dir_list = os.listdir(dirname)
+			dir_list = os.listdir(dirname)#save file names from dir
 			listing = "<br>"
-			for filename in dir_list:
+			for filename in dir_list:#Format file names to be in HTML format
 				listing = listing + "<a href="+filename+" download>" + filename + "</a><br>"
 			clientFD.send("""<html>
                         	<head>
@@ -57,24 +58,24 @@ class WebServer:
                         	+listing+
                         	""" </body>
                         	</html>
-                        	""")
+                        	""")#Write file names to client
 		except OSError:
 			print "in display error"
-			self.displayDir(os.getcwd(),clientFD)
+			self.displayDir(os.getcwd(),clientFD)#display current dir if error
 		
 	def displayFile(self, filename, clientFD):
 		try:
 			print "in display file"
-			f = open(filename)
-                	outputData = f.read()
+			f = open(filename)#open file client requested
+                	outputData = f.read()#save content
                 	f.close()
-                	clientFD.send(outputData)
+                	clientFD.send(outputData)#write content
                 	clientFD.close()
 		except IOError:
-                	self.displayDir(os.getcwd(),clientFD) 
+                	self.displayDir(os.getcwd(),clientFD)#display current dir if error 
 
 while True:
-	server = WebServer("1000","127.0.0.1")
+	server = WebServer()
 	try:
 		server.StartServer()
 	except KeyboardInterrupt:
